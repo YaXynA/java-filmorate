acf53cefd412ce6cee3e2d81bdfa35e959d70ef7
XynA <YaXymma@yandex.ru>
2023-09-02 18:41:48 +0300

    Создан для короткого стек трейса ошибок

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String handleValidationException(ValidationException ex) {
+        return ex.getMessage();
+    }
+}
